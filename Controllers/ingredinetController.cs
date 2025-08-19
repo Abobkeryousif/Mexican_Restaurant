22ed326e8346fbf43e13b99820c0e8b5cb948062
bakry <bakryhassona7@gmail.com>
2025-08-19 10:35:46 +0300

    Complete CRUD Opration And View For Ingredients

--- Controllers/ingredinetController.cs
+++ Controllers/ingredinetController.cs
@@ -18,5 +18,69 @@ namespace Mexican_Restaurant.Controllers
         {
             return View(await ingredinet.GetAllAsync());
         }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await ingredinet.GetByIdAsync(id, new QueryOption<Ingredient>() { Includes = "ProductIngredients.Product" }));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind("IngredientId , Name")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredinet.AddAsync(ingredient);
+                return RedirectToAction("Index");
+            }
+
+            return View(ingredient);
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Delete(int id) 
+        {
+            return View(await ingredinet.GetByIdAsync(id, new QueryOption<Ingredient> { Includes = "ProductIngredients.Product" }));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Ingredient ingredient)
+        {
+            await ingredinet.DeleteAsync(ingredient.IngredientId);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await ingredinet.GetByIdAsync(id, new QueryOption<Ingredient> { Includes = "ProductIngredients.Product" }));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Ingredient Ingredient)
+        {
+            if (ModelState.IsValid) 
+            { 
+            await ingredinet.UpdateAsync(Ingredient);
+            return RedirectToAction("Index");
+            }
+            return View(Ingredient);
+        }
     }
 }
+
+
+
+
+
+
+
